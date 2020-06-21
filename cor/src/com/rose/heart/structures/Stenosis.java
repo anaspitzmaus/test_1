86663ebf99e75222436df930206cae0b14848390
user2 <user2@CLIENT2.KARDIO.local>
2020-06-21 10:13:22 +0200

    21062020_Stenosis

---
+++ cor/src/com/rose/heart/structures/Stenosis.java
@@ -0,0 +1,17 @@
+package com.rose.heart.structures;
+
+public class Stenosis {
+	Integer grading;
+	
+	protected Integer getGrading() {
+		return grading;
+	}
+
+	protected void setGrading(Integer grading) {
+		this.grading = grading;
+	}
+
+	public Stenosis() {
+		// TODO Auto-generated constructor stub
+	}
+}
